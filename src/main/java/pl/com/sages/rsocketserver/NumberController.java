952a0e1ed9e0681198e884920ad8bcffe3009e85
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-09-29 23:45:07 +0200

    duplex channel

--- src/main/java/pl/com/sages/rsocketserver/NumberController.java
+++ src/main/java/pl/com/sages/rsocketserver/NumberController.java
@@ -4,7 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.function.Function;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,5 +24,12 @@ public class NumberController {
     @MessageMapping("accept.number")
     public Mono<Void> acceptNumber(Mono<Integer> number) {
         return number.flatMap(numberService::acceptNumber);
+    }
+
+    @MessageMapping("chat.number")
+    public Flux<Integer> chatNumber(Flux<Integer> inbound) {
+        inbound.subscribe(i -> log.info("inbound: " + i), e -> log.warn("", e));
+        Flux<Integer> outbound = Flux.interval(Duration.ofSeconds(5)).map(l -> (int)(long)l);
+        return outbound;
     }
 }
